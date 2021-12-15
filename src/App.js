9694e8123cac091f68bdf252aab03efd1f7bed33
vestigialcode <dibyajiban232@gmail.com>
2021-12-15 23:15:42 +0530

    added commits

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 import ReactMarkdown from 'react-markdown';
 //import ReactDom from 'react-dom';
 
 function App() {
   const[state,setState]=useState("")
+  const [changeit,setChangeit]= useState("")
+  useEffect(()=>document.title=changeit)
+  const chanto=(e)=>{
+    setState(e.target.value)
+    setChangeit(e.target.value)
+    
+  }
 
   return (
-    <div >Markdown Viewer made with React
-<textarea placeholder='write here #ok or **react is awesome**' className='textarea' onChange={(e)=>setState(e.target.value)}># hello from react </textarea>
+    <div className='footer'>Markdown Viewer made with React
+<textarea placeholder='write here #ok or **reactisawesome**' className='textarea' onChange={chanto}># hello from react </textarea>
 <div >
   <h2>Converted Markdown appear here</h2>
 <ReactMarkdown>{state}</ReactMarkdown>
