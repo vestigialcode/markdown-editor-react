52dbada4df138acff9b12c56b3ea2fc47d4b45ce
vestigialcode <dibyajiban232@gmail.com>
2021-12-15 23:02:44 +0530

    added markdown

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React,{useState} from 'react';
 import './App.css';
+import ReactMarkdown from 'react-markdown';
+//import ReactDom from 'react-dom';
 
 function App() {
   const[state,setState]=useState("")
 
   return (
-    <div >Type letters to make them uppercase
-<textarea className='textarea' onChange={(e)=>setState(e.target.value)}> </textarea>
+    <div >Markdown Viewer made with React
+<textarea placeholder='write here #ok or **react is awesome**' className='textarea' onChange={(e)=>setState(e.target.value)}># hello from react </textarea>
 <div >
-{state.toUpperCase()}
+  <h2>Converted Markdown appear here</h2>
+<ReactMarkdown>{state}</ReactMarkdown>
 </div>
+<footer className='footer'>Made with ❤️ by<a href='https://github.com/vestigialcode'> Vestigialcode</a></footer>
     </div>
   );
 }
